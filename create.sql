8954fbae1bebe5c328a61ec8aa69af71ca0f1d10
Suilan Maia <suilansmaia@gmail.com>
2026-03-30 18:18:13 -0300

    feat: add postgres database with some data

---
+++ create.sql
@@ -0,0 +1,35 @@
+-- Ao criar um migration, o banco de dados é recriado, então é necessário criar o schema e as tabelas novamente
+create extension if not exists "uuid-ossp";
+
+drop schema if exists branas cascade;
+create schema branas;
+
+-- Contém o Serviço: 
+create table branas.contract (
+    -- Numero do Serviço
+    id_contract uuid not null default uuid_generate_v4() primary key,
+    -- Descrição do serviço
+    description text,
+    -- Valor do serviço
+    ammount numeric,
+    -- Por quanto tempo o serviço é prestado, em meses
+    period integer,
+    -- Quando começou a prestação do serviço
+    date timestamp
+);
+
+-- Pagamento: dependendo do tipo de regime, o pagamento importa
+create table branas.payment (
+    id_payment uuid not null default uuid_generate_v4() primary key,
+    id_contract uuid not null references branas.contract(id_contract),
+    -- Valor pago
+    amount numeric,
+    -- Quando o pagamento foi realizado
+    date timestamp
+);
+
+insert into branas.contract (id_contract, description, ammount, period, date) 
+values ('389e9182-a739-46cf-860e-258bb6602fc6', 'Prestação de serviços escolares', 6000, 12, '2022-01-01T10:00:00');
+
+insert into branas.payment (id_payment, id_contract, amount, date) 
+values ('06517b35-e821-4e0c-bf07-174b681c35e0', '389e9182-a739-46cf-860e-258bb6602fc6', 6000, '2022-01-05T10:00:00');
