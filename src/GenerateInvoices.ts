69673ec7d9254333f9ff44dce1112a08fbd8e8db
Suilan Maia <suilansmaia@gmail.com>
2026-04-01 19:42:40 -0300

    feat: connect and query the postgres database

--- src/GenerateInvoices.ts
+++ src/GenerateInvoices.ts
@@ -1,5 +1,10 @@
+import pgp from "pg-promise";
+
 export default class GenerateInvoices {
     async execute() {
+        const connection = pgp()("postgres://postgres:postgres@localhost:5432/branas");
+        const contracts = await connection.query("select * from branas.contract", []);
+        console.log(contracts);
         return [];
     }
 }
